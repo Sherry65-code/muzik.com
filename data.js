ccb60aa3a8acf36569b74217fd06ad7ebc47a087
Parambir Singh <70046342+Sherry65-code@users.noreply.github.com>
2022-09-03 21:54:00 +0530

    Add files via upload

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
-songName = ["Master of Puppets","Sweet Child O Mine", "Socha Hai","Zinda Hai Toh","Yellow"];
-songUrl = ["master of puppets.mp3", "sweet child o mine.mp3", "socha hai.mp3","zinda hai toh.mp3","yellow.mp3"];
-author = ["Metallica", "Guns N Roses","Rock On", "Siddarth Mahadevan","Coldplay"];
-songImg = ["master of puppets.jpg", "sweet child o mine.jpg","socha hai.jpg","zinda hai toh.jpg","yellow.jpg"];
-totalLength = 5;
+songName = ["Master of Puppets","Sweet Child O Mine", "Socha Hai","Zinda Hai Toh","Yellow", "Belupacito", "Pasoori", "Excuses", "Crazy Train", "Taki Taki", "Smells Like Teen Spirit"];
+songUrl = ["master of puppets.mp3", "sweet child o mine.mp3", "socha hai.mp3","zinda hai toh.mp3","yellow.mp3","belupacito.mp3","pasoori.mp3","excuses.mp3", "crazy train.mp3", "taki taki.mp3", "smells like teen spirit.mp3"];
+author = ["Metallica", "Guns N Roses","Rock On", "Siddarth Mahadevan","Coldplay","Beluga","Ali Sethi","AP Dhillon","Ozzy Ozbourne", "DJ Snake", "Nirvana"];
+songImg = ["master of puppets.jpg", "sweet child o mine.jpg","socha hai.jpg","zinda hai toh.jpg","yellow.jpg","belupacito.webp","pasoori.jpeg", "excuses.jpeg","crazy train.jpeg", "taki taki.jpg", "smells like teen spirit.jpg"];
+totalLength = 11;
