58f138f69177372036a1f52d4f5f214d649d0158
Parambir Singh <70046342+Sherry65-code@users.noreply.github.com>
2022-09-13 20:11:03 +0530

    Update data.js

--- data.js
+++ data.js
@@ -2,4 +2,4 @@ songName = ["Master of Puppets","Sweet Child O Mine", "Socha Hai","Zinda Hai Toh
 songUrl = ["master of puppets.mp3", "sweet child o mine.mp3", "socha hai.mp3","zinda hai toh.mp3","yellow.mp3","belupacito.mp3","pasoori.mp3","excuses.mp3", "crazy train.mp3", "taki taki.mp3", "smells like teen spirit.mp3", "one.mp3"];
 author = ["Metallica", "Guns N Roses","Rock On", "Siddarth Mahadevan","Coldplay","Beluga","Ali Sethi","AP Dhillon","Ozzy Ozbourne", "DJ Snake", "Nirvana", "Metallica"];
 songImg = ["master of puppets.jpg", "sweet child o mine.jpg","socha hai.jpg","zinda hai toh.jpg","yellow.jpg","belupacito.webp","pasoori.jpeg", "excuses.jpeg","crazy train.jpeg", "taki taki.jpg", "smells like teen spirit.jpg","one.jpg"];
-totalLength = 11;
+totalLength = 12;
